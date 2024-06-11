e76b468e3b5790cf6cce7204854df546234b9c6e
Clemens Weglau <clemens.weglau@smail.inf.h-brs.de>
2024-06-11 11:37:40 +0200

    [microservices] define ports and add HealthController

---
+++ ms-auxiliary-systems/src/main/java/org/ooka/msauxiliarysystems/controller/HealthController.java
@@ -0,0 +1,16 @@
+package org.ooka.msauxiliarysystems.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/health")
+public class HealthController {
+
+    @GetMapping("/alive")
+    public ResponseEntity isAlive() {
+        return ResponseEntity.ok().build();
+    }
+}
