ba52fd218e1c9d8543344dced3a0c08d98e753ad
Clemens Weglau <clemens.weglau@smail.inf.h-brs.de>
2024-06-22 14:33:11 +0200

    misc

--- ms-auxiliary-systems/src/main/java/org/ooka/msauxiliarysystems/controller/RequestController.java
+++ ms-auxiliary-systems/src/main/java/org/ooka/msauxiliarysystems/controller/RequestController.java
@@ -3,8 +3,6 @@ package org.ooka.msauxiliarysystems.controller;
 import org.ooka.msauxiliarysystems.Algorithm;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
