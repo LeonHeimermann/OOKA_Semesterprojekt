037df943d84687d21b85d61fc314466ac63d8638
Clemens Weglau <c.weglau@posteo.de>
2024-09-03 16:06:04 +0200

    post dummy config to bff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,30 @@ function App() {
     const [showSaveModal, setShowSaveModal] = React.useState(false);
 
     function saveConfiguration(title, description) {
-        alert(title, description)
+        fetch("http://localhost:8085/configuration",{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                title: title,
+                description: description,
+                // testdata - replace with real configuration later
+                engineType: "M96_2000",
+                engineConfiguration: "V10",
+                startingSystem: "AIR_STARTER",
+                auxiliaryPTO: "ALTERNATOR",
+                oilSystem: "DIVERTER_VALVE",
+                fuelSystem: "MONITORING_FUEL_LEAKAGE",
+                coolingSystem: "SEAWATER_PIPING",
+                exhaustSystem: "EXHAUST_DISCHARGE_ROTATABLE",
+                mountingSystem: "RESILIANT_MOUNTS",
+                engineManagementSystem: "SOCIETY_REGULATIONS",
+                monitoringSystem: "BLUE_VISION",
+                powerTransmission: "TORSIONALLY_RESILLIANT_COUPLING",
+                gearboxOption: "HYDRAULIC_PUMP_DRIVES"
+            })
+        }).then(response => alert(response.status))
     }
 
     return (
